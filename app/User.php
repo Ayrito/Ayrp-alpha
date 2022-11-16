f03bd97373fa96c79219b4a95b1a78031fd59b34
Ayrito <115940690+Ayrito@users.noreply.github.com>
2022-11-16 21:57:17 +0100

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -2,11 +2,9 @@
 
 namespace App;
 
-use App\Mail\NewUserWelcomeMail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -18,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'username', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
@@ -38,32 +36,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function ($user) {
-            $user->profile()->create([
-                'title' => $user->username,
-            ]);
-
-            Mail::to($user->email)->send(new NewUserWelcomeMail());
-        });
-    }
-
-    public function posts()
-    {
-        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
-    }
-
-    public function following()
-    {
-        return $this->belongsToMany(Profile::class);
-    }
-
-    public function profile()
-    {
-        return $this->hasOne(Profile::class);
-    }
 }
