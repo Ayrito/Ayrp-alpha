4b48b6b52d3a6326dcba2908e5265a1c2c3eca13
Ayrito <115940690+Ayrito@users.noreply.github.com>
2022-11-16 22:08:25 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-use App\Mail\NewUserWelcomeMail;
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -31,15 +29,4 @@ Route::get('/article', function () {
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
-
-Route::post('follow/{user}', 'FollowsController@store');
-
-Route::get('/home', 'PostsController@index');
-Route::get('/p/create', 'PostsController@create');
-Route::post('/p', 'PostsController@store');
-Route::get('/p/{post}', 'PostsController@show');
-
-Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
-Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
-Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
+Route::get('/home', 'HomeController@index')->name('home');
