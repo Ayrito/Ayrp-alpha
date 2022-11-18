09904f46fd99664ce713f39d83c8745a9a2989c9
Ayrito <115940690+Ayrito@users.noreply.github.com>
2022-11-18 14:05:29 +0100

    Update index.blade.php

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
+@include('inc.head')
 
-@section('content')
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
@@ -45,4 +45,5 @@
         @endforeach
     </div>
 </div>
-@endsection
+
+@include('inc.footer')
